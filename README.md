4373e45cd9d797ff1ff102bcc3c2a3b1e01e8fe7
Akshat Gupta <33231334+akshatvg@users.noreply.github.com>
2021-07-09 11:56:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Agora Change Remote User Role
+
+[![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/akshatvg/Agora-Change-Remote-User-Role?logo=github&style=social)](https://github.com/akshatvg/) [![GitHub last commit](https://img.shields.io/github/last-commit/akshatvg/Agora-Change-Remote-User-Role?style=social&logo=git)](https://github.com/akshatvg/) [![GitHub stars](https://img.shields.io/github/stars/akshatvg/Agora-Change-Remote-User-Role?style=social)](https://github.com/akshatvg/Agora-Change-Remote-User-Role/stargazers) [![GitHub forks](https://img.shields.io/github/forks/akshatvg/Agora-Change-Remote-User-Role?style=social&logo=git)](https://github.com/akshatvg/Agora-Change-Remote-User-Role/network)
+
+Sample app to change role of a remote user in a live stream using the RTM SDK.
+
+<p align="center">
+<a href="https://changerole.akshatvg.com">
+<img src="./favicon.png" width="60px" alt="Agora.io"/>
+</a>
+</p>
+
+![Generic badge](https://img.shields.io/badge/Change-Role-orange) 
+
+[![Generic badge](https://img.shields.io/badge/view-demo-purple)](https://changerole.akshatvg.com)
+
+## Features
+
+- Livestreaming
+- Changing role of user to audience or host
+
+## Useful Links
+
+- [Deployed Website](https://changerole.akshatvg.com)
+- [Agora.io Website](https://www.agora.io/)
+- [SDK NG Documentation](https://agoraio-community.github.io/AgoraWebSDK-NG/api/en/index.html)
+- [Sample Video Call App](https://github.com/AgoraIO-Community/AgoraWebSDK-NG/tree/master/Demo/basicVideoCall)
+- [Agora RTM SDK Documentation](https://docs.agora.io/en/Real-time-Messaging/API%20Reference/RTM_web/v1.0.0/index.html)
+- [RTM Quickstart Guide](https://docs.agora.io/en/Real-time-Messaging/messaging_web?platform=Web)
+- [Guided Blog](https://medium.com/@akshatvg/change-role-of-remote-host-in-a-live-streaming-web-app-8c5b3e0a651f)
+
+## Requirements
+
+[![GitHub top language](https://img.shields.io/github/languages/top/akshatvg/Agora-Change-Remote-User-Role?logo=javascript&style=social)](https://github.com/akshatvg/)
+
+The source code of this project is written in **`HTML`, `CSS`, `JavaScript`, `Agora RTM SDK` and the `Agora SDK NG`**. So, you would require the Agora SDKs and a valid App Id to run this project.
+
+## Instructions
+
+[![chrome](https://img.shields.io/badge/Open-index.html-lightgrey.svg?logo=google-chrome&style=popout&logoColor=red)](https://changerole.akshatvg.com)
+
+Extract the folder and run `index.html`
+
+
+## Need help?
+
+
+Feel free to contact me via [Facebook](https://www.facebook.com/akshatvg).
+
+Invite me to connect on [LinkedIn](https://www.linkedin.com/in/akshatvg/).
+
+[![Facebook](https://img.shields.io/badge/Facebook-add-blue.svg?logo=facebook&logoColor=white)](https://www.facebook.com/akshatvg) [![Quora](https://img.shields.io/badge/Quora-ask-red.svg?logo=quora)](https://www.quora.com/profile/Akshat-Gupta-279) [![Instagram](https://img.shields.io/badge/Instagram-follow-purple.svg?logo=instagram&logoColor=white)](https://www.instagram.com/akshatvg/) [![Snapchat](https://img.shields.io/badge/Snapchat-add-yellow.svg?logo=snapchat&logoColor=white)](https://www.snapchat.com/add/akshatvg) [![Medium](https://img.shields.io/badge/Medium-follow-black.svg?logo=medium&logoColor=white)](https://medium.com/@akshatvg)
+
+
+```bash
+
+
+
+ _____ _                 _     __   __            
+|_   _| |               | |    \ \ / /            
+  | | | |__   __ _ _ __ | | __  \ V /___  _   _   
+  | | | '_ \ / _` | '_ \| |/ /   \ // _ \| | | |  
+  | | | | | | (_| | | | |   <    | | (_) | |_| |  
+  \_/ |_| |_|\__,_|_| |_|_|\_\   \_/\___/ \__,_|  
+                                                  
+                                                  
+______                                            
+|  ___|                                           
+| |_ ___  _ __                                    
+|  _/ _ \| '__|                                   
+| || (_) | |                                      
+\_| \___/|_|                                      
+                                                  
+                                                  
+______      _               _   _               _ 
+| ___ \    (_)             | | | |             | |
+| |_/ / ___ _ _ __   __ _  | |_| | ___ _ __ ___| |
+| ___ \/ _ \ | '_ \ / _` | |  _  |/ _ \ '__/ _ \ |
+| |_/ /  __/ | | | | (_| | | | | |  __/ | |  __/_|
+\____/ \___|_|_| |_|\__, | \_| |_/\___|_|  \___(_)
+                     __/ |                        
+                    |___/                         
+
+ 
+
+
+```
+
+## License
+
+**MIT &copy; [Akshat Gupta](https://github.com/akshatvg/Agora-Change-Remote-User-Role/blob/master/LICENSE)**
+
+[![GitHub license](https://img.shields.io/github/license/akshatvg/Agora-Change-Remote-User-Role?style=social&logo=github)](https://github.com/akshatvg/Agora-Change-Remote-User-Role/blob/master/LICENSE) [![Twitter Follow](https://img.shields.io/twitter/follow/akshatvg?style=social)](https://twitter.com/akshatvg)
+
+---------
+
+```javascript
+
+if (youEnjoyed) {
+    starThisRepository();
+}
+
+```
+
+-----------
+
---
+++ index.js
@@ -0,0 +1,290 @@
+// create Agora client
+var client = AgoraRTC.createClient({
+    mode: "live",
+    codec: "vp8"
+});
+
+// RTM Global Vars
+var isLoggedIn = false;
+
+var localTracks = {
+    videoTrack: null,
+    audioTrack: null
+};
+var remoteUsers = {};
+// Agora client options
+var options = {
+    appid: $("#appid").val(),
+    channel: null,
+    uid: null,
+    token: null,
+    accountName: null,
+    role: "audience"
+};
+
+// Host join
+$("#host-join").click(function (e) {
+    RTMJoin();
+    options.role = "host";
+})
+
+// Audience join
+$("#audience-join").click(function (e) {
+    RTMJoin();
+    options.role = "audience";
+})
+
+// Join form submission
+$("#join-form").submit(async function (e) {
+    e.preventDefault();
+    $("#host-join").attr("disabled", true);
+    $("#audience-join").attr("disabled", true);
+    try {
+        options.appid = $("#appid").val();
+        options.token = $("#token").val();
+        options.channel = $("#channel").val();
+        options.accountName = $('#accountName').val();
+        await join();
+    } catch (error) {
+        console.error(error);
+    } finally {
+        $("#leave").attr("disabled", false);
+    }
+})
+
+// Leave click
+$("#leave").click(function (e) {
+    leave();
+})
+
+async function join() {
+    // create Agora client
+    client.setClientRole(options.role);
+    if (options.role === "audience") {
+        // add event listener to play remote tracks when remote user publishs.
+        client.on("user-published", handleUserPublished);
+        client.on("user-unpublished", handleUserUnpublished);
+    }
+    // join the channel
+    options.uid = await client.join(options.appid, options.channel, options.token || null);
+    if (options.role === "host") {
+        // create local audio and video tracks
+        localTracks.audioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+        localTracks.videoTrack = await AgoraRTC.createCameraVideoTrack();
+        // play local video track
+        localTracks.videoTrack.play("local-player");
+        $("#local-player-name").text(`localTrack(${options.uid})`);
+        // publish local tracks to channel
+        await client.publish(Object.values(localTracks));
+        console.log("Successfully published.");
+    }
+}
+
+// Leave
+async function leave() {
+    for (trackName in localTracks) {
+        var track = localTracks[trackName];
+        if (track) {
+            track.stop();
+            track.close();
+            localTracks[trackName] = undefined;
+        }
+    }
+    // remove remote users and player views
+    remoteUsers = {};
+    $("#remote-playerlist").html("");
+    // leave the channel
+    await client.leave();
+    $("#local-player-name").text("");
+    $("#host-join").attr("disabled", false);
+    $("#audience-join").attr("disabled", false);
+    $("#leave").attr("disabled", true);
+    console.log("Client successfully left channel.");
+}
+
+async function RTMJoin() {
+    // Create Agora RTM client
+    const clientRTM = AgoraRTM.createInstance($("#appid").val(), {
+        enableLogUpload: false
+    });
+    var accountName = $('#accountName').val();
+    // Login
+    clientRTM.login({
+        uid: accountName
+    }).then(() => {
+        console.log('AgoraRTM client login success. Username: ' + accountName);
+        isLoggedIn = true;
+        // RTM Channel Join
+        var channelName = $('#channel').val();
+        channel = clientRTM.createChannel(channelName);
+        channel.join().then(() => {
+            console.log('AgoraRTM client channel join success.');
+            // Get all members in RTM Channel
+            channel.getMembers().then((memberNames) => {
+                console.log("------------------------------");
+                console.log("All members in the channel are as follows: ");
+                console.log(memberNames);
+                var newHTML = $.map(memberNames, function (singleMember) {
+                    if (singleMember != accountName) {
+                        return (`<li class="mt-2">
+                  <div class="row">
+                      <p>${singleMember}</p>
+                   </div>
+                   <div class="mb-4">
+                     <button class="text-white btn btn-live mx-3 remoteHost hostOn" id="remoteAudio-${singleMember}">Make Host</button>
+                     <button class="text-white btn btn-live remoteAudience audienceOn" id="remoteVideo-${singleMember}">Make Audience</button>
+                    </div>
+                 </li>`);
+                    }
+                });
+                $("#insert-all-users").html(newHTML.join(""));
+            });
+            // Send peer-to-peer message for changing role to host
+            $(document).on('click', '.remoteHost', function () {
+                fullDivId = $(this).attr('id');
+                peerId = fullDivId.substring(fullDivId.indexOf("-") + 1);
+                console.log("Remote host button pressed.");
+                let peerMessage = "host";
+                clientRTM.sendMessageToPeer({
+                        text: peerMessage
+                    },
+                    peerId,
+                ).then(sendResult => {
+                    if (sendResult.hasPeerReceived) {
+                        console.log("Message has been received by: " + peerId + " Message: " + peerMessage);
+                    } else {
+                        console.log("Message sent to: " + peerId + " Message: " + peerMessage);
+                    }
+                })
+            });
+            // Send peer-to-peer message for changing role to audience
+            $(document).on('click', '.remoteAudience', function () {
+                fullDivId = $(this).attr('id');
+                peerId = fullDivId.substring(fullDivId.indexOf("-") + 1);
+                console.log("Remote audience button pressed.");
+                let peerMessage = "audience";
+                clientRTM.sendMessageToPeer({
+                        text: peerMessage
+                    },
+                    peerId,
+                ).then(sendResult => {
+                    if (sendResult.hasPeerReceived) {
+                        console.log("Message has been received by: " + peerId + " Message: " + peerMessage);
+                    } else {
+                        console.log("Message sent to: " + peerId + " Message: " + peerMessage);
+                    }
+                })
+            });
+            // Display messages from peer
+            clientRTM.on('MessageFromPeer', function ({
+                text
+            }, peerId) {
+                console.log(peerId + " changed your role to " + text);
+                if (text == "host") {
+                    leave();
+                    options.role = "host";
+                    console.log("Role changed to host.");
+                    client.setClientRole("host");
+                    join();
+                    $("#host-join").attr("disabled", true);
+                    $("#audience-join").attr("disabled", true);
+                } else if (text == "audience") {
+                    leave();
+                    options.role = "audience";
+                    console.log("Role changed to audience.");
+                    client.setClientRole("audience");
+                    join();
+                    $("#host-join").attr("disabled", true);
+                    $("#audience-join").attr("disabled", true);
+                }
+            })
+            // Display channel member joined updated users
+            channel.on('MemberJoined', function () {
+                // Get all members in RTM Channel
+                channel.getMembers().then((memberNames) => {
+                    console.log("New member joined so updated list is: ");
+                    console.log(memberNames);
+                    var newHTML = $.map(memberNames, function (singleMember) {
+                        if (singleMember != accountName) {
+                            return (`<li class="mt-2">
+                      <div class="row">
+                          <p>${singleMember}</p>
+                       </div>
+                       <div class="mb-4">
+                         <button class="text-white btn btn-live mx-3 remoteHost hostOn" id="remoteAudio-${singleMember}">Make Host</button>
+                         <button class="text-white btn btn-live remoteAudience audienceOn" id="remoteVideo-${singleMember}">Make Audience</button>
+                        </div>
+                     </li>`);
+                        }
+                    });
+                    $("#insert-all-users").html(newHTML.join(""));
+                });
+            })
+            // Display channel member left updated users
+            channel.on('MemberLeft', function () {
+                // Get all members in RTM Channel
+                channel.getMembers().then((memberNames) => {
+                    console.log("A member left so updated list is: ");
+                    console.log(memberNames);
+                    var newHTML = $.map(memberNames, function (singleMember) {
+                        if (singleMember != accountName) {
+                            return (`<li class="mt-2">
+                      <div class="row">
+                          <p>${singleMember}</p>
+                       </div>
+                       <div class="mb-4">
+                         <button class="text-white btn btn-live mx-3 remoteHost hostOn" id="remoteAudio-${singleMember}">Make Host</button>
+                         <button class="text-white btn btn-live remoteAudience audienceOn" id="remoteVideo-${singleMember}">Make Audience</button>
+                        </div>
+                     </li>`);
+                        }
+                    });
+                    $("#insert-all-users").html(newHTML.join(""));
+                });
+            });
+        }).catch(error => {
+            console.log('AgoraRTM client channel join failed: ', error);
+        }).catch(err => {
+            console.log('AgoraRTM client login failure: ', err);
+        });
+    });
+    // Logout
+    document.getElementById("leave").onclick = async function () {
+        console.log("Client logged out of RTM.");
+        await clientRTM.logout();
+    }
+}
+
+// Subscribe to a remote user
+async function subscribe(user, mediaType) {
+    const uid = user.uid;
+    await client.subscribe(user, mediaType);
+    console.log("Successfully subscribed.");
+    if (mediaType === 'video') {
+        const player = $(`
+      <div id="player-wrapper-${uid}">
+        <p class="player-name">remoteUser(${uid})</p>
+        <div id="player-${uid}" class="player"></div>
+      </div>
+    `);
+        $("#remote-playerlist").append(player);
+        user.videoTrack.play(`player-${uid}`);
+    }
+    if (mediaType === 'audio') {
+        user.audioTrack.play();
+    }
+}
+
+// Handle user publish
+function handleUserPublished(user, mediaType) {
+    const id = user.uid;
+    remoteUsers[id] = user;
+    subscribe(user, mediaType);
+}
+
+// Handle user unpublish
+function handleUserUnpublished(user) {
+    const id = user.uid;
+    delete remoteUsers[id];
+    $(`#player-wrapper-${id}`).remove();
+}
